58a994f6c45e12aa1f13b1bb9752f387ea599615
André Santos <andrefs@andrefs.com>
2021-02-10 22:16:07 +0000

    Resource.getNext to pop next resource to crawl

--- models/Resource.js
+++ models/Resource.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
 
 const resourceSchema = new mongoose.Schema({
   url: {
@@ -6,22 +8,35 @@ const resourceSchema = new mongoose.Schema({
     index: true,
     unique: true
   },
-  crawled: {
-    type: Boolean,
-    index: true,
-    default: true
-  },
   projects: [{
     type: mongoose.Types.ObjectId,
     ref: 'Project',
     default: [],
     index: true
   }],
-  totalTriples: {
+  status: {
+    type: String,
+    enum: ['unvisited', 'done', 'crawling', 'error'],
+    default: 'unvisited'
+  },
+  priority: {
     type: Number,
-    default: 0
-  }
+    default: 5
+  },
+  totalTriples: Number
 }, {timestamps: true});
 
+
+resourceSchema.statics.getNext = async function(projectName){
+  let query = {status: 'unvisited'};
+  if(projectName){
+    const proj = await require('./Project').findOne({name: projectName});
+    if(!proj){
+      throw 'no such project';
+    }
+    query.projects = {_id: proj._id};
+  }
+  return this.findOneAndUpdate(query, {'$set': {status: 'crawling'}}, {new: true});
+};
 
 module.exports = mongoose.model('Resource', resourceSchema);
