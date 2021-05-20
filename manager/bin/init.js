1bc0a3253dbd7d704ea71f308cde7f902973da5e
André Santos <andrefs@andrefs.com>
2021-05-20 13:10:00 +0100

    minor

--- manager/bin/init.js
+++ manager/bin/init.js
@@ -4,19 +4,20 @@ const Resource = require('../models/Resource');
 const seeds = [
   //'http://data.nobelprize.org/data/country/Saint_Lucia',
   //'http://dbpedia.org/resource/Berlin',
-  //'http://dbpedia.org/resource/Emmental_cheese',
-  //'http://dbpedia.org/resource/Cheddar_cheese',
-  //'http://dbpedia.org/resource/Mozzarella',
-  //'http://dbpedia.org/resource/Roquefort',
-  //'http://dbpedia.org/resource/Gouda_cheese'
+
+  'http://dbpedia.org/resource/Emmental_cheese',
+  'http://dbpedia.org/resource/Cheddar_cheese',
+  'http://dbpedia.org/resource/Mozzarella',
+  'http://dbpedia.org/resource/Roquefort',
+  'http://dbpedia.org/resource/Gouda_cheese'
 
 
-  'http://www.wikidata.org/entity/Q41225', // Big Ben
-  'http://www.wikidata.org/entity/Q243', // Eiffel Tower
-  'http://www.wikidata.org/entity/Q9141', // Taj Mahal
-  'http://www.wikidata.org/entity/Q82425', // Brandenburg Gate
-  'http://www.wikidata.org/entity/Q5788', // Petra
-  'http://www.wikidata.org/entity/Q10285', // Colosseum
+  //'http://www.wikidata.org/entity/Q41225', // Big Ben
+  //'http://www.wikidata.org/entity/Q243', // Eiffel Tower
+  //'http://www.wikidata.org/entity/Q9141', // Taj Mahal
+  //'http://www.wikidata.org/entity/Q82425', // Brandenburg Gate
+  //'http://www.wikidata.org/entity/Q5788', // Petra
+  //'http://www.wikidata.org/entity/Q10285', // Colosseum
 
 ];
 
