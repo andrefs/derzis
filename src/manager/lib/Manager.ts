8b27e403c8afe67c435cb3713dec7356a3ad85de
André Santos <andrefs@andrefs.com>
2022-06-22 22:48:40 +0100

    wip

--- src/manager/lib/Manager.ts
+++ src/manager/lib/Manager.ts
@@ -340,7 +340,7 @@ export default class Manager {
     if(this.finished > 5){
       log.info('No current processes running, starting new process');
       Process.startNext();
-      //console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX this should be the end!', this.finished, workerAvail, assignedCheck, assignedCrawl, this.jobs.count(), this.beingSaved.count());
+      console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX this should be the end!', this.finished, workerAvail, assignedCheck, assignedCrawl, this.jobs.toString(), this.beingSaved);
     }
   }
 };
