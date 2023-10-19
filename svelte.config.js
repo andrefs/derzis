e8eb19deb86b85cd3ecd55b48f2ead3a61e92787
André Santos <andrefs@andrefs.com>
2023-10-19 12:03:15 +0100

    manager folder

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,29 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+    files: {
+      assets: 'manager/static',
+      hooks: {
+        client: 'manager/src/hooks.client',
+        server: 'manager/src/hooks.server',
+      },
+      lib: 'manager/src/lib',
+      routes: 'manager/src/routes',
+      appTemplate: 'manager/src/app.html',
+      errorTemplate: 'manager/src/error.html',
+
+    }
+  },
+
 };
 
 export default config;
