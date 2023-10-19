75fcd00f24dcf2519cb9015576d6d0b85192f91b
André Santos <andrefs@andrefs.com>
2023-10-20 00:15:06 +0100

    wip

--- models/src/Counter.ts
+++ models/src/Counter.ts
@@ -1,33 +1,33 @@
-//import { createLogger } from '@derzis/common';
-//const log = createLogger('Counter');
+import { createLogger } from '@derzis/common';
+const log = createLogger('Counter');
 import { type ReturnModelType, getModelForClass, index, prop } from '@typegoose/typegoose';
 
 @index({ name: 1 })
 class CounterClass {
-	@prop({ type: String, required: true })
-	name!: string;
+  @prop({ type: String, required: true })
+  name!: string;
 
-	@prop({ type: Number, required: true, default: 0 })
-	value!: number;
+  @prop({ type: Number, required: true, default: 0 })
+  value!: number;
 
-	public static async genId(this: ReturnModelType<typeof CounterClass>, name: string) {
-		const c = await this.findOneAndUpdate(
-			{ name },
-			{ $inc: { value: 1 } },
-			{
-				upsert: true,
-				returnDocument: 'after',
-				lean: true,
-				projection: 'value'
-			}
-		);
-		//log.debug(`Generated job id ${c!.value}`);
-		return c!.value;
-	}
+  public static async genId(this: ReturnModelType<typeof CounterClass>, name: string) {
+    const c = await this.findOneAndUpdate(
+      { name },
+      { $inc: { value: 1 } },
+      {
+        upsert: true,
+        returnDocument: 'after',
+        lean: true,
+        projection: 'value'
+      }
+    );
+    log.debug(`Generated job id ${c!.value}`);
+    return c!.value;
+  }
 }
 
 const Counter = getModelForClass(CounterClass, {
-	schemaOptions: { timestamps: true, collection: 'counters' }
+  schemaOptions: { timestamps: true, collection: 'counters' }
 });
 
 export { Counter, CounterClass };
