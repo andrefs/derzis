8f997f80c336a2c0abdc302086412009c65c2286
André Santos <andrefs@andrefs.com>
2023-10-24 20:55:20 +0100

    wip

--- manager/src/routes/api/processes/+server.ts
+++ manager/src/routes/api/processes/+server.ts
@@ -2,6 +2,7 @@ import { newProcess } from '$lib/process-helper';
 import { Process, type ProcessClass } from '@derzis/models';
 import { json } from '@sveltejs/kit';
 import type { RecursivePartial } from '@derzis/common';
+import type { RequestEvent } from './$types';
 
 export async function GET() {
 	const ps: ProcessClass[] = await Process.find().lean();
@@ -10,7 +11,7 @@ export async function GET() {
 	return json({ processes: _ps });
 }
 
-export async function POST({ request }) {
+export async function POST({ request }: RequestEvent) {
 	const { process }: { process: RecursivePartial<ProcessClass> } = await request.json();
 	const proc = await newProcess(process);
 
