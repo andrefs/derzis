a59a0049b4b99e80374b8c75cce8afb7dc1230bb
André Santos <andrefs@andrefs.com>
2023-10-27 18:17:54 +0100

    edit process form

---
+++ manager/src/routes/processes/[pid]/edit/+page.server.ts
@@ -0,0 +1,36 @@
+import { error } from '@sveltejs/kit';
+import { secondsToString } from '$lib/utils';
+import { Process, ProcessClass, Resource } from '@derzis/models';
+
+export async function load({ params }) {
+	const _p: ProcessClass | null = await Process.findOne({ pid: params.pid }).lean();
+	if (!_p) {
+		throw error(404, {
+			message: 'Not found'
+		});
+	}
+
+	const lastResource = await Resource.findOne().sort({ updatedAt: -1 });
+	const timeRunning = lastResource
+		? (lastResource!.updatedAt.getTime() - _p.createdAt.getTime()) / 1000
+		: null;
+	const p = {
+		..._p,
+		createdAt: _p.createdAt?.toISOString(),
+		updatedAt: _p.updatedAt?.toISOString() || _p.createdAt,
+		timeRunning: timeRunning ? secondsToString(timeRunning) : '',
+		notification: {
+			..._p.notification,
+			email: _p?.notification?.email
+				?.replace(/(?<=.).*?(?=.@)/, (x) => '*'.repeat(x.length))
+				?.replace(/^..(?=@)/, '**')
+		}
+	};
+
+	return p;
+}
+
+//export const actions = {
+//	async addStep({ params, body }) { },
+//	async update({ params, body }) { }
+//};
---
+++ manager/src/routes/processes/[pid]/edit/+page.svelte
@@ -0,0 +1,192 @@
+<script>
+	export let data;
+	import { enhance } from '$app/forms';
+	import {
+		Container,
+		Row,
+		Col,
+		InputGroup,
+		Accordion,
+		Button,
+		Tooltip,
+		FormGroup,
+		AccordionItem,
+		Label,
+		Input,
+		InputGroupText
+	} from 'sveltestrap';
+</script>
+
+<Container>
+	<header style="padding-bottom: 1rem">
+		<h2>Edit crawling process</h2>
+	</header>
+
+	<Container>
+		<Row>
+			<Col>
+				<Accordion>
+					<AccordionItem active header="Add new crawling step">
+						<form
+							id="new-crawl-step"
+							method="POST"
+							use:enhance={() => {
+								return ({ update }) => update({ reset: false });
+							}}
+						>
+							<FormGroup>
+								<Row>
+									<Col sm={2}>
+										<Label>Additional seeds:</Label>
+									</Col>
+									<Col sm={10}>
+										<InputGroup>
+											<Input
+												id="seeds"
+												name="seeds"
+												type="textarea"
+												rows={3}
+												form="new-crawl-step"
+											/>
+											<Tooltip target="resources-tt">One resource URL per line</Tooltip>
+											<InputGroupText id="resources-tt">?</InputGroupText>
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+
+							<FormGroup>
+								<Row>
+									<Col sm={2}>
+										<Label>Max length:</Label>
+									</Col>
+									<Col sm={2}>
+										<InputGroup>
+											<Input
+												min="1"
+												max="20"
+												step="1"
+												value="3"
+												type="number"
+												name="maxPathLength"
+											/>
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+
+							<FormGroup>
+								<Row>
+									<Col sm={2}>
+										<Label>Max props:</Label>
+									</Col>
+									<Col sm={2}>
+										<InputGroup>
+											<Input min="1" max="5" step="1" value="2" type="number" name="maxPathProps" />
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+
+							<FormGroup>
+								<Row>
+									<Col sm={2}>
+										<Label>Predicate white list:</Label>
+									</Col>
+									<Col sm={10}>
+										<InputGroup>
+											<Input
+												id="whiteList"
+												name="whiteList"
+												type="textarea"
+												rows={3}
+												form="new-crawl-step"
+											/>
+											<Tooltip target="white-list-tt">One resource URL per line</Tooltip>
+											<InputGroupText id="white-list-tt">?</InputGroupText>
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+
+							<FormGroup>
+								<Row>
+									<Col sm={2}>
+										<Label>Predicate black list:</Label>
+									</Col>
+									<Col sm={10}>
+										<InputGroup>
+											<Input
+												id="blackList"
+												name="blackList"
+												type="textarea"
+												rows={3}
+												form="new-crawl-step"
+											/>
+											<Tooltip target="black-list-tt">One resource URL per line</Tooltip>
+											<InputGroupText id="black-list-tt">?</InputGroupText>
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+							<FormGroup>
+								<Col sm={10}>
+									<Button color="primary" type="submit">Add new step</Button>
+								</Col>
+							</FormGroup>
+						</form>
+					</AccordionItem>
+					<AccordionItem header="Edit other settings">
+						<form
+							id="edit-proc"
+							method="POST"
+							use:enhance={() => {
+								return ({ update }) => update({ reset: false });
+							}}
+						>
+							<FormGroup>
+								<Row>
+									<Col sm={1}>
+										<Label>Email:</Label>
+									</Col>
+									<Col sm={{ size: 6, offset: 1 }}>
+										<InputGroup>
+											<Input name="email" placeholder={data.notification.email} />
+											<Tooltip target="email-tt"
+												>We'll only send you 3 emails: confirming your process has been added; when
+												your process starts; and when your process finishes. No SPAM</Tooltip
+											>
+											<InputGroupText id="email-tt">?</InputGroupText>
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+							<FormGroup>
+								<Row>
+									<Col sm={1}>
+										<Label>Webhook:</Label>
+									</Col>
+									<Col sm={{ size: 6, offset: 1 }}>
+										<InputGroup>
+											<Input name="webhook" />
+											<Tooltip target="webhook-tt"
+												>An HTTP callback which can be used to send notifications when the status of
+												this process changes.</Tooltip
+											>
+											<InputGroupText id="webhook-tt">?</InputGroupText>
+										</InputGroup>
+									</Col>
+								</Row>
+							</FormGroup>
+							<FormGroup>
+								<Col sm={10}>
+									<Button color="primary" type="submit">Save</Button>
+								</Col>
+							</FormGroup>
+						</form>
+					</AccordionItem>
+				</Accordion>
+			</Col>
+		</Row>
+	</Container>
+</Container>
