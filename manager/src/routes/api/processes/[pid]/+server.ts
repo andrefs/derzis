0ac9941bd9fc459e15c03f2e579ab012ced419dc
André Santos <andrefs@andrefs.com>
2025-02-03 16:28:12 +0000

    add route to api for process info

---
+++ manager/src/routes/api/processes/[pid]/+server.ts
@@ -0,0 +1,18 @@
+import { error, json } from '@sveltejs/kit';
+import { createLogger } from 'vite';
+const log = createLogger();
+import * as processHelper from '$lib/process-helper';
+import type { RequestEvent } from './$types';
+
+export const GET = async ({ params, request }: RequestEvent) => {
+  log.info(`GET ${request.url}`);
+  const p = processHelper.info(params.pid);
+
+  if (!p) {
+    throw error(404, {
+      message: 'Not found'
+    });
+  }
+
+  return json({ ok: true, data: p });
+};
