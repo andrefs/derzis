e37420da5cdd82eb9fc0f1539e6390fce0b0168f
André Santos <andrefs@andrefs.com>
2026-01-01 21:00:07 +0000

    fix ports for docker and local

--- manager/vite.config.ts
+++ manager/vite.config.ts
@@ -1,37 +1,40 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { loadEnv } from 'vite';
 
 const aliases = ['common', 'worker', 'models', 'config'];
 
-export default defineConfig({
-  resolve: {
-    alias: [
-      ...aliases.map((alias) => ({
-        find: `@derzis/${alias}`,
-        replacement: path.resolve(__dirname, `../${alias}/src/index.ts`)
-      })),
-      {
-        find: `@derzis/manager`,
-        replacement: path.resolve(__dirname, `src/index.ts`)
-      }
-    ]
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
+export default defineConfig(() => {
+  const envDir = path.resolve(__dirname, '../');
+  const env = loadEnv('', envDir, '')
+  return {
+    envDir,
+    resolve: {
+      alias: [
+        ...aliases.map((alias) => ({
+          find: `@derzis/${alias}`,
+          replacement: path.resolve(__dirname, `../${alias}/src/index.ts`)
+        })),
+        {
+          find: `@derzis/manager`,
+          replacement: path.resolve(__dirname, `src/index.ts`)
+        }
+      ]
     },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true
+      },
 
-  },
-  plugins: [sveltekit()],
-  test: {
-    include: ['manager/**/*.{test,spec}.{js,ts}']
-  },
-  server: {
-    allowedHosts: true
-    //port: 59982
-  },
-  preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000
-  },
+    },
+    plugins: [sveltekit()],
+    test: {
+      include: ['manager/**/*.{test,spec}.{js,ts}']
+    },
+    server: {
+      allowedHosts: true as true,
+      port: env.DERZIS_MNG_PORT ? parseInt(env.DERZIS_MNG_PORT) : 3000
+    },
+  }
 });
