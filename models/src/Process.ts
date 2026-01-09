bdd39bd0963c1237ef5795dff9c7fb474e2dab4c
André Santos <andrefs@andrefs.com>
2026-01-09 00:45:44 +0000

    wip

--- models/src/Process.ts
+++ models/src/Process.ts
@@ -137,7 +137,6 @@ export class StepClass {
   this.notification.ssePath = ssePath;
 })
 class ProcessClass extends Document {
-  _id!: Types.ObjectId | string;
   createdAt?: Date;
   updatedAt?: Date;
 
