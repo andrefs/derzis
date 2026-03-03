f933d35a8d01625b2e6e6569a646deebfba5e458
André Santos <andrefs@andrefs.com>
2026-03-03 17:11:12 +0000

    chore(eslint): enhance TypeScript linting rules across all packages
    - Add TypeScript project configuration to eslint.config.js files
    - Add strict unsafe-type rules: no-unsafe-assignment, no-unsafe-member-access, no-unsafe-call, no-unsafe-return
    - Restrict use of "as", "satisfies", and "!" non-null assertions
    - Add eslint script to all package.json files

--- models/eslint.config.js
+++ models/eslint.config.js
@@ -2,6 +2,9 @@ const js = require('@eslint/js');
 const tseslint = require('@typescript-eslint/eslint-plugin');
 const tsparser = require('@typescript-eslint/parser');
 const prettier = require('eslint-config-prettier');
+const path = require('path');
+
+const projectRoot = __dirname;
 
 module.exports = [
 	js.configs.recommended,
@@ -11,7 +14,9 @@ module.exports = [
 			parser: tsparser,
 			parserOptions: {
 				ecmaVersion: 2020,
-				sourceType: 'module'
+				sourceType: 'module',
+				project: './tsconfig.json',
+				tsconfigRootDir: projectRoot
 			}
 		},
 		plugins: {
@@ -20,7 +25,26 @@ module.exports = [
 		rules: {
 			'@typescript-eslint/await-thenable': 'error',
 			'@typescript-eslint/no-floating-promises': 'error',
-			'@typescript-eslint/no-explicit-any': 'error'
+			'@typescript-eslint/no-explicit-any': 'error',
+			'@typescript-eslint/no-unsafe-assignment': 'error',
+			'@typescript-eslint/no-unsafe-member-access': 'error',
+			'@typescript-eslint/no-unsafe-call': 'error',
+			'@typescript-eslint/no-unsafe-return': 'error',
+			'no-restricted-syntax': [
+				'error',
+				{
+					selector: 'TSAsExpression',
+					message: 'Use of "as" type casting is not allowed. Use proper typing instead.'
+				},
+				{
+					selector: 'TSSatisfiesExpression',
+					message: 'Use of "satisfies" type casting is not allowed. Use proper typing instead.'
+				},
+				{
+					selector: 'TSNonNullExpression',
+					message: 'Use of "!" non-null assertion is not allowed. Use proper null checks instead.'
+				}
+			]
 		}
 	},
 	prettier
--- worker/eslint.config.js
+++ worker/eslint.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const projectRoot = __dirname;
+
 module.exports = {
 	extends: [
 		'eslint:recommended',
@@ -8,7 +11,9 @@ module.exports = {
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: './tsconfig.json',
+		tsconfigRootDir: projectRoot
 	},
 	env: {
 		es2017: true,
@@ -18,6 +23,25 @@ module.exports = {
 	rules: {
 		'@typescript-eslint/await-thenable': 'error',
 		'@typescript-eslint/no-floating-promises': 'error',
-		'@typescript-eslint/no-explicit-any': 'error'
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-unsafe-assignment': 'error',
+		'@typescript-eslint/no-unsafe-member-access': 'error',
+		'@typescript-eslint/no-unsafe-call': 'error',
+		'@typescript-eslint/no-unsafe-return': 'error',
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'TSAsExpression',
+				message: 'Use of "as" type casting is not allowed. Use proper typing instead.'
+			},
+			{
+				selector: 'TSSatisfiesExpression',
+				message: 'Use of "satisfies" type casting is not allowed. Use proper typing instead.'
+			},
+			{
+				selector: 'TSNonNullExpression',
+				message: 'Use of "!" non-null assertion is not allowed. Use proper null checks instead.'
+			}
+		]
 	}
 };
