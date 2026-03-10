7b9dde2c98fa07ba0b622276a161ebc1fcf36581
André Santos <andrefs@andrefs.com>
2026-03-10 18:51:47 +0000

    fix: update configuration handling

--- config/src/index.ts
+++ config/src/index.ts
@@ -20,7 +20,7 @@ export default {
     },
     // this must be defined before ever running Derzis
     // and cannot ever be changed!
-    pathType: process.env.DERZIS_PATH_TYPE || 'endpoint',
+    pathType: process.env.DERZIS_PATH_TYPE || 'traversal',
     pauseAfterJob: Number(process.env.MANAGER_PAUSE_AFTER_JOB) || 2,
     predicates: {
       branchingFactor: {
